44d59cbe87f2850ede199edd345b89be25a7f086
Gab_km <the25thcromosome@gmail.com>
2011-05-19 00:20:52 +0900

    [from now] Thu May 19 00:20:52     2011
    diff --git a/MyFirstGitHub/FizzBuzzMain.fs b/MyFirstGitHub/FizzBuzzMain.fs
    new file mode 100644
    index 0000000..c5dd867
    --- /dev/null
    +++ b/MyFirstGitHub/FizzBuzzMain.fs
    @@ -0,0 +1,8 @@
    +﻿// Learn more about F# at http://fsharp.net
    +
    +module FizzBuzzMain
    +
    +let fizzbuzz num =
    +    if num = 3
    +        then "Fizz"
    +        else "Buzz";;
    \ No newline at end of file
    diff --git a/MyFirstGitHub/FizzBuzzTest.fs b/MyFirstGitHub/FizzBuzzTest.fs
    new file mode 100644
    index 0000000..258c557
    --- /dev/null
    +++ b/MyFirstGitHub/FizzBuzzTest.fs
    @@ -0,0 +1,12 @@
    +﻿module FizzBuzzTest
    +
    +open NUnit.Framework
    +open FizzBuzzMain
    +
    +[<Test>]
    +let test_fizzbuzz_3を適用したらFizzって返す () =
    +    Assert.That ("Fizz", Is.EqualTo (fizzbuzz 3));;
    +
    +[<Test>]
    +let test_fizzbuzz_5を適用したらBuzzって返す () =
    +    Assert.That ("Buzz", Is.EqualTo (fizzbuzz 5));;
    \ No newline at end of file

---
+++ MyFirstGitHub/FizzBuzzMain.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.net
+
+module FizzBuzzMain
+
+let fizzbuzz num =
+    if num = 3
+        then "Fizz"
+        else "Buzz";;
---
+++ MyFirstGitHub/FizzBuzzTest.fs
@@ -0,0 +1,12 @@
+﻿module FizzBuzzTest
+
+open NUnit.Framework
+open FizzBuzzMain
+
+[<Test>]
+let test_fizzbuzz_3を適用したらFizzって返す () =
+    Assert.That ("Fizz", Is.EqualTo (fizzbuzz 3));;
+
+[<Test>]
+let test_fizzbuzz_5を適用したらBuzzって返す () =
+    Assert.That ("Buzz", Is.EqualTo (fizzbuzz 5));;
